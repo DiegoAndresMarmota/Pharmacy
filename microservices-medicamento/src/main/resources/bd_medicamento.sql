85747b300cbda7a82c064e9244a4775adcfc3b8b
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2024-01-15 13:57:23 -0300

    micro-medicamento-0.3.5 (#8)
    * Creación de Lab.sql y Med.sql
    
    * micro-medicamento-0.3.3 (#7)
    
    * Creación de Lab.sql y Med.sql (#5)
    
    * micro-medicamento-0.3.2
    
    * INSERT INTO laboratorios, medicamentos

---
+++ microservices-medicamento/src/main/resources/bd_medicamento.sql
@@ -0,0 +1,70 @@
+INSERT INTO medicamentos (
+    nombre_medicamento,
+    nombre_laboratorio,
+    principio_activo,
+    registro_sanitario,
+    concentracion,
+    forma_farmaceutica,
+    laboratorio_id
+    )
+    VALUES (
+        'Paracetamol 500 mg',
+        'Andromaco S.A',
+        'Paracetamol',
+        'F-8660/21'
+        '500 MG',
+        'Comprimido',
+        '141'
+    );
+INSERT INTO medicamentos (
+    nombre_medicamento,
+    nombre_laboratorio,
+    principio_activo,
+    registro_sanitario,
+    concentracion,
+    forma_farmaceutica,
+    laboratorio_id
+    )
+    VALUES (
+        'Eutirox 88 mcg',
+        'Merck',
+        'Levotiroxina Sodica',
+        'F-20857/19'
+        '88 MCG',
+        'Comprimido',
+        '117'
+    );
+INSERT INTO medicamentos (
+    nombre_medicamento,
+    nombre_laboratorio,
+    principio_activo,
+    registro_sanitario,
+    concentracion,
+    forma_farmaceutica,
+    laboratorio_id
+    )
+    VALUES (
+        'Metformina Clorhidrato 750 mg',
+        'Ascend Laboratories S.p.A.',
+        'Metformina Clorhidrato',
+        'F-23334/17'
+        '750 MG',
+        'Comprimido',
+        '109'
+    );
+-- INSERT INTO laboratorios (
+--     nombre_laboratorio,
+--     rut_laboratorio,
+--     registroISP,
+--     direccion_laboratorio,
+--     laboratorio_id
+--     )
+--     VALUES (
+--         '',
+--         '',
+--         ''
+--         '',
+--         ''
+--     );
+    
+
