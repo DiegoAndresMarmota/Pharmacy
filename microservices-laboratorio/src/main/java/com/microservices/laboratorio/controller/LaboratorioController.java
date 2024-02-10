e78d7a57505d24266a2d628b5111c8cb95fad96d
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2024-02-09 23:39:06 -0300

    micro-security-0.8.1 (#14)
    * Creación de SessionRegistry, SessionInformation <Laboratorio, Medicamento>

--- microservices-laboratorio/src/main/java/com/microservices/laboratorio/controller/LaboratorioController.java
+++ microservices-laboratorio/src/main/java/com/microservices/laboratorio/controller/LaboratorioController.java
@@ -1,6 +1,11 @@
 package com.microservices.laboratorio.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +21,17 @@ import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/api/laboratorio")
-public class LaboratorioController {
+public class LaboratorioController<SessionRegistryLaboratorio, SessionInformationLaboratorio> {
 
     @Autowired
     private ILaboratorioService laboratorioService;
+
+    @Autowired
+    private SessionRegistryLaboratorio sessionRegistry;
+
+    @SuppressWarnings("unused")
+    @Autowired
+    private SessionInformationLaboratorio SessionInformation;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -40,7 +52,32 @@ public class LaboratorioController {
     @GetMapping("/search-all-medicamento/{laboratorioId}")
     public ResponseEntity<?> findAllMedicamentoByLaboratorio(Long laboratorioId){
         return ResponseEntity.ok(laboratorioService.findAllMedicamentoByLaboratorio(laboratorioId));
-    
     }
 
+    @GetMapping("/session-laboratorio")
+    public ResponseEntity<?> getDetailsSession(){
+
+        String sessionId = "";
+        User userObject = null;
+
+        List<Object> sessions = sessionRegistry.getAllPrincipals();
+
+        for (Object session : sessions) {
+            if(session instanceof User) {
+                userObject = (User) session;
+            }
+
+            List<SessionInformationLaboratorio> sessionInformations = sessionRegistry.getAllSessions(session, false);
+        
+            for(SessionInformationLaboratorio  sessionInformation : sessionInformations){
+                sessionId = sessionInformation.getSessionId();
+            }
+        }
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("sessionId", sessionId);
+        response.put("sessionUser", userObject);
+
+        return ResponseEntity.ok(response);
+    }
 }
