b87da23b5d996c735e824289174d53beb5da91fc
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2024-03-02 22:20:53 -0300

    Creación de authorizeHttpRequests(auth -> { ... })

---
+++ microservices-admin/src/main/java/com/microservices/admin/security/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.microservices.admin.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .csrf(config -> config.disable())
+            .authorizeHttpRequests(auth -> {
+                auth.requestMatchers("/admin").hasRole("ADMIN");
+                auth.requestMatchers("/user").hasRole("USER");
+                auth.requestMatchers("/").permitAll();
+                auth.requestMatchers("/medicamentos").permitAll();
+                auth.requestMatchers("/higiene-y-cuidado-personal").permitAll();
+                auth.requestMatchers("/dermocosmetica").permitAll();
+                auth.requestMatchers("/vitaminas-y-suplementos").permitAll();
+                auth.requestMatchers("/infantil-y-maternidad").permitAll();
+                auth.requestMatchers("/dispositivos-medicos").permitAll();
+                auth.requestMatchers("/bebidas-y-alimentos").permitAll();
+                auth.requestMatchers("/mundo-mascota").permitAll();
+                auth.requestMatchers("/recetario-magistral").permitAll();
+                auth.anyRequest().authenticated();
+            })
+            .sessionManagement(session -> {
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            })
+            .build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+            User.withUsername("JPabloQuezada").password("jpq1901").roles("ADMIN").build());
+        return manager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder) throws Exception {
+        return httpSecurity
+                .getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder)
+                .and().build();
+    }
+
+
+
+
+}
