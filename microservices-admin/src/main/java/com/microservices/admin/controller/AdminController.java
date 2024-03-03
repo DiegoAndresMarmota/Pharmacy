a258e4bcbeb5bd0e00a111d8fe87268b82ef0c98
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2024-03-02 22:22:31 -0300

    Creación de authorizeHttpRequests(auth -> { ... }) (#17)

--- microservices-admin/src/main/java/com/microservices/admin/controller/AdminController.java
+++ microservices-admin/src/main/java/com/microservices/admin/controller/AdminController.java
@@ -4,6 +4,7 @@ import java.util.stream.Collectors;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,9 @@ import jakarta.validation.Valid;
 
 @RestController
 public class AdminController {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private AdminRepository adminRepository;
@@ -39,7 +43,7 @@ public class AdminController {
 
         AdminEntity adminEntity = AdminEntity.builder()
                 .adminname(createAdminDTO.getAdminname())
-                .password(createAdminDTO.getPassword())
+                .password(passwordEncoder.encode(createAdminDTO.getPassword()))
                 .email(createAdminDTO.getEmail())
                 .roles(roles)
                 .build();
