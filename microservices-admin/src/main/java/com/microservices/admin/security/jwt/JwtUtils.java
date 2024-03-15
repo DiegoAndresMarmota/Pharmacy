84cdec06b4919afab6e038445107c0ebec41844e
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2024-03-14 23:49:02 -0300

    Creación de Auth[ ]

--- microservices-admin/src/main/java/com/microservices/admin/security/jwt/JwtUtils.java
+++ microservices-admin/src/main/java/com/microservices/admin/security/jwt/JwtUtils.java
@@ -3,6 +3,18 @@ package com.microservices.admin.security.jwt;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Date;
+import java.util.function.Function;
+import java.security.Key;
+
+@Slf4j
 @Component
 public class JwtUtils {
 
@@ -12,4 +24,49 @@ public class JwtUtils {
     @Value("${jwt.time.expiration}")
     private String timeExpiration;
     
+    @SuppressWarnings("deprecation")
+    public String generateAccessToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(timeExpiration)))
+                .signWith(getSignatureKey(), io.jsonwebtoken.SignatureAlgorithm.HS256)
+                .compact();
+            }
+
+    public Key getSignatureKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean isTokenValid(String token){
+        try {
+            Jwts.parserBuilder()
+                .setSigningKey(getSignatureKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+            return true;
+        } catch (Exception e){
+            log.error("Token inválido, error: ".concat(e.getMessage()));
+            return false;
+        }
+    }
+
+    public Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignatureKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T getClaim(String token, Function<Claims, T> claimsTFunction){
+        final Claims claims = extractAllClaims(token);
+        return claimsTFunction.apply(claims);
+    }
+
+    public String getAdminFromToken(String token){
+        return getClaim(token, Claims::getSubject);
+    }
 }
